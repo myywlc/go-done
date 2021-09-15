cce9ecb2a8af9fec114ad0022bf1b7a5b715b4ec
jiangchengbiao <jiangchengbiao@aidigger.com>
2021-09-15 11:02:48 +0800

    init

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
 	r := gin.Default()
 
 	// Ping test
@@ -29,27 +27,11 @@ func setupRouter() *gin.Engine {
 		}
 	})
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
 		"foo":  "bar", // user:foo password:bar
 		"manu": "123", // user:manu password:123
 	}))
 
-	/* example curl for /admin with basicauth header
-	      Zm9vOmJhcg== is base64("foo:bar")
-
-	   	curl -X POST \
-	     	http://localhost:8080/admin \
-	     	-H 'authorization: Basic Zm9vOmJhcg==' \
-	     	-H 'content-type: application/json' \
-	     	-d '{"value":"bar"}'
-	*/
 	authorized.POST("admin", func(c *gin.Context) {
 		user := c.MustGet(gin.AuthUserKey).(string)
 
@@ -70,5 +52,8 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":3000")
+	err := r.Run("127.0.0.1:3000")
+	if err != nil {
+		return
+	}
 }
